84fc296f369b1d5288c3cf968b96fbd0ab90cec1
Jared Grove <jaredkgrove@gmail.com>
2019-02-28 05:15:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,9 +15,13 @@ def third_element(array)
 end
 
 def last_element(array)
-  array.last
+  array[-1]
 end
 
 def first_element_with_array_methods(array)
   array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
 end
